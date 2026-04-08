9f42a65e6c00b4e6d98635cb3418039a41b2b181
lowkeydergoat <skordov@student.tgm.ac.at>
2026-04-08 09:56:10 +0200

    Delete backend directory

--- backend/subcategories/update.php
+++
@@ -1,8 +0,0 @@
-<?php
-require "../db.php";
-$data = json_decode(file_get_contents("php://input"), true);
-
-$stmt = $pdo->prepare("UPDATE subcategories SET name=? WHERE id=? AND userid=?");
-$stmt->execute([$data["name"], $data["id"], $data["userid"]]);
-
-echo json_encode(["success" => true]);
